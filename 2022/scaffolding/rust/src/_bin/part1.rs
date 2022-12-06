7d9e8f8691c18084e2c11e3a0a3da464c1dc2ef2
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-12-06 13:43:31 +0000

    Restructure day-6 and Rust scaffolding

--- 2022/scaffolding/rust/src/_bin/part1.rs
+++
@@ -1,5 +0,0 @@
-use day_DAYNUM::{bin::process_part1, get_input};
-
-fn main() {
-    println!("{}", process_part1(&get_input()));
-}
--- 2022/scaffolding/rust/src/bin.rs
+++ 2022/scaffolding/rust/src/bin.rs
@@ -1,16 +1,27 @@
 use crate::*;
 
-pub fn process_part1(s: &str) -> u32 {}
+pub fn process_part1(s: &str) -> u32 {
+    0
+}
 
-pub fn process_part2(s: &str) -> u32 {}
+pub fn process_part2(s: &str) -> u32 {
+    0
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn process_part1_test() {}
+    const INPUT: &str = "";
 
     #[test]
-    fn process_part2_test() {}
+    fn process_part1_test() {
+        assert_eq!(process_part1(INPUT), 1);
+    }
+
+    #[test]
+    #[ignore]
+    fn process_part2_test() {
+        assert_eq!(process_part2(INPUT), 1);
+    }
 }
