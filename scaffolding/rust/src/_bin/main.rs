90fb75e196c88a74e447bff00d30a5c9bbc48bd4
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-12-09 10:50:06 +0000

    Add correct answer tests and nom dependency to scaffolding

--- scaffolding/rust/src/_bin/main.rs
+++ scaffolding/rust/src/_bin/main.rs
@@ -1,8 +1,4 @@
-use day_DAYNUM::{bin::process_part1, bin::process_part2};
-
-fn get_input() -> String {
-    std::fs::read_to_string("./input.txt").unwrap()
-}
+use day_DAYNUM::bin::{get_input, process_part1, process_part2};
 
 fn main() {
     println!("Part 1: {:?}", process_part1(&get_input()));
