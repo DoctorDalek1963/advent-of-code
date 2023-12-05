ecfbcd0c63fffb813c35f01301358f6df1966f65
DoctorDalek1963 <dyson.dyson@icloud.com>
2023-12-05 15:59:33 +0000

    Add dhat heap profiling

--- 2023/day-5/src/_bin/main.rs
+++ 2023/day-5/src/_bin/main.rs
@@ -1,6 +1,13 @@
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 use day_5::bin::{get_input, process_part1, process_part2};
 
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     println!("Part 1: {}", process_part1(&get_input()));
     println!("Part 2: {}", process_part2(&get_input()));
 }
