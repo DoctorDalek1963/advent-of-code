2c28b330e00a35556f6a1cc77ed4ddbd02bd3c2e
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-06-04 20:29:09 +0100

    Update root flake to be more generic

--- flake.nix
+++ flake.nix
@@ -1,17 +1,56 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  description = "My solutions to Advent of Code problems in a variety of languages";
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        fd
-        just
-        rustup
-        (python3.withPackages (p: [p.aocd]))
-      ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.pre-commit-hooks.flakeModule
+      ];
+
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        system,
+        ...
+      }: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            fd
+            just
+            (python3.withPackages (p: [p.aocd]))
+          ];
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        # See https://flake.parts/options/pre-commit-hooks-nix and
+        # https://github.com/cachix/git-hooks.nix/blob/master/modules/hooks.nix
+        # for all the available hooks and options
+        pre-commit.settings.hooks = {
+          check-added-large-files.enable = true;
+          check-merge-conflicts.enable = true;
+          check-toml.enable = true;
+          check-vcs-permalinks.enable = true;
+          check-yaml.enable = true;
+          end-of-file-fixer.enable = true;
+          trim-trailing-whitespace.enable = true;
+
+          alejandra.enable = true;
+          rustfmt.enable = true;
+        };
+      };
+    };
 }
