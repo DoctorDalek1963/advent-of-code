122e51447e895a87f4c8ee00aadef221d603a5e9
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-06-10 12:30:23 +0100

    Make tests deliberately fail in Elixir scaffolding

--- scaffolding/elixir/test/dayDAYNUM_test.exs
+++ scaffolding/elixir/test/dayDAYNUM_test.exs
@@ -2,12 +2,13 @@ defmodule DayDAYNUMTest do
   use ExUnit.Case
   doctest DayDAYNUM, import: true
 
+  @tag :skip
   test "process part 1" do
-    assert DayDAYNUM.process_part1(Util.get_input()) === 0
+    assert DayDAYNUM.process_part1(Util.get_input()) === :answer
   end
 
   @tag :skip
   test "process part 2" do
-    assert DayDAYNUM.process_part2(Util.get_input()) === 0
+    assert DayDAYNUM.process_part2(Util.get_input()) === :answer
   end
 end
